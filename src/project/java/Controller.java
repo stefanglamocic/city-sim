f13804dec63835e111515fbc1b722cbdc55db574
Stefan Glamocic <keksglamocic@gmail.com>
2021-06-17 16:18:24 +0200

    first commit

---
+++ src/project/java/Controller.java
@@ -0,0 +1,29 @@
+package project.java;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+    @FXML
+    private FlowPane fpTop;
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    public void initialize(){
+//        Image image = new Image("assets/cars/car2.png");
+//        Image image2 = new Image("assets/cars/car1.png");
+//        Image image3 = new Image("assets/trains/train.png");
+//        fpTop.getChildren().add(new ImageView(image));
+//        fpTop.getChildren().add(new ImageView(image2));
+
+//        gridPane.add(new ImageView(image3), 0, 0);
+//        gridPane.add(new ImageView(image), 0, 1);
+//        gridPane.add(new ImageView(image2), 1, 1);
+//        gridPane.add(new ImageView(image), 2, 1);
+
+    }
+}
---
+++ src/project/java/Main.java
@@ -0,0 +1,23 @@
+package project.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Java 2021");
+        primaryStage.setScene(new Scene(root, 1200, 900));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
